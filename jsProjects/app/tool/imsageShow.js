19d549581aa6c11a8a649bf20ec8f5b02345bb62
qcy1121 <qcy1121@gmail.com>
2014-05-30 23:12:42 +0800

    update image show

--- jsProjects/app/tool/imsageShow.js
+++ jsProjects/app/tool/imsageShow.js
@@ -1,10 +1,12 @@
 ﻿(function($){
 	$.fn.imageLazyLoading= function(options){
+		var self = $(this);
 		var opts = $.extendd({
 			container:$(window),
 			data:null,
 			dataUrl:'data-url',
-			fadeIn:0
+			fadeIn:0,
+			noLoadedImg:'noLoadedImg.jpg'
 		},options||{});
 		if(!opts.data){
 			//if(opts.data-url)
@@ -13,33 +15,41 @@
 			var obj = $(this);
 				if(obj.nodeName.toLowerCase()=="img"){
 				data.push({
-					url:$(this).attr(opts.dataUrl),
-					obj :obj
+					url:obj.attr(opts.dataUrl),
+					obj :obj//,
+					//updated:false;
 				});
 			}
 			});
-			opts.data=[];
+			opts.data=data;
+		}else{
+			$.each(data,function(i,e){
+				var obj = $("<img>");
+				obj.attr("src",opts.noLoadedImg);
+				//obj.addClass(opts.noLoadedImg);
+				e.obj = obj;
+				self.append(obj);
+			});
 		}
 		var cter = opts.container;
 		var lazyLoading = function(){
 			$.each(data,function(i,e){
 				var obj = e.obj,
 				url = e.url;
-				if(obj){
+				if(!obj.updated){
 					var height = cter.height(),
 					scrollTop = cter.scrollTop(),
 					selfTop = obj.offset().top;
 					if((selfTop-scrollTop)>0&&(selfTop-scrolltop)<height){
 						obj.fadeIn(0);
 						obj.attr("src",url);
-						
-					e.obj = null;
+						obj.updated = true;
 					}
 				}
 			});
 			
 		}
 		cter.bind("scroll",lazyLoading);
-		
+		lazyLoading();
 	};
 })(jQuery)
