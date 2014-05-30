467b67461c38ee23f3779774ab189eaa6778e0d2
qcy1121 <qcy1121@gmail.com>
2014-05-30 23:09:07 +0800

    add image show

---
+++ jsProjects/app/tool/imsageShow.js
@@ -0,0 +1,45 @@
+﻿(function($){
+	$.fn.imageLazyLoading= function(options){
+		var opts = $.extendd({
+			container:$(window),
+			data:null,
+			dataUrl:'data-url',
+			fadeIn:0
+		},options||{});
+		if(!opts.data){
+			//if(opts.data-url)
+			var data = [];
+			$(this).each(function(){
+			var obj = $(this);
+				if(obj.nodeName.toLowerCase()=="img"){
+				data.push({
+					url:$(this).attr(opts.dataUrl),
+					obj :obj
+				});
+			}
+			});
+			opts.data=[];
+		}
+		var cter = opts.container;
+		var lazyLoading = function(){
+			$.each(data,function(i,e){
+				var obj = e.obj,
+				url = e.url;
+				if(obj){
+					var height = cter.height(),
+					scrollTop = cter.scrollTop(),
+					selfTop = obj.offset().top;
+					if((selfTop-scrollTop)>0&&(selfTop-scrolltop)<height){
+						obj.fadeIn(0);
+						obj.attr("src",url);
+						
+					e.obj = null;
+					}
+				}
+			});
+			
+		}
+		cter.bind("scroll",lazyLoading);
+		
+	};
+})(jQuery)
