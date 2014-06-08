037f8354ab73b62a89652b19883317c243950ed8
qcy1121 <qcy1121@gmail.com>
2014-06-08 23:21:28 +0800

    imageshow update ,not complete

--- jsProjects/app/tool/imsageShow.js
+++ jsProjects/app/tool/imsageShow.js
@@ -15,39 +15,43 @@
 		};
 		return { "left": left, "top": top, "right": right, "bottom": bottom };
 	};
+    var prepareOptions=function(self,options){
+        var opts = $.extend({
+            container:$(window),
+            data:null,
+            dataUrl:'data-url',
+            fadeIn:0,
+            noLoadedImg:'noLoadedImg.jpg'
+        },options||{});
+        if(!opts.data){
+            //if(opts.data-url)
+            var data = [];
+            $(this).each(function(){
+                var obj = $(this);
+                if(obj[0].nodeName.toLowerCase()=="img"){
+                    data.push({
+                        url:obj.attr(opts.dataUrl),
+                        obj :obj//,
+                        //updated:false;
+                    });
+                }
+            });
+            opts.data=data;
+        }else{
+            $.each(opts.data,function(i,e){
+                if(!e)return;
+                var obj = $("<img>");
+                obj.attr("src",opts.noLoadedImg);
+                //obj.addClass(opts.noLoadedImg);
+                e.obj = obj;
+                self.append(obj);
+            });
+        }
+        return opts;
+    }
 	$.fn.imageLazyLoading= function(options){
 		var self = $(this);
-		var opts = $.extend({
-			container:$(window),
-			data:null,
-			dataUrl:'data-url',
-			fadeIn:0,
-			noLoadedImg:'noLoadedImg.jpg'
-		},options||{});
-		if(!opts.data){
-			//if(opts.data-url)
-			var data = [];
-			$(this).each(function(){
-			var obj = $(this);
-				if(obj[0].nodeName.toLowerCase()=="img"){
-				data.push({
-					url:obj.attr(opts.dataUrl),
-					obj :obj//,
-					//updated:false;
-				});
-			}
-			});
-			opts.data=data;
-		}else{
-			$.each(opts.data,function(i,e){
-                if(!e)return;
-				var obj = $("<img>");
-				obj.attr("src",opts.noLoadedImg);
-				//obj.addClass(opts.noLoadedImg);
-				e.obj = obj;
-				self.append(obj);
-			});
-		}
+		var opts=prepareOptions(self,options);
 		var cter = opts.container,cterRect;
         var isNeedUpdate=function(obj){
             var rect = obj.getRect();
@@ -91,4 +95,13 @@
 		cter.on("resize",checkLoading);
 		lazyLoading();
 	};
+    $.fn.newGalleryView=function(){
+
+
+    };
+    $.fn.useGalleryView=function(options){
+        var self = $(this);
+        var opts = prepareOptions(self,options)
+        opts.container.galleryView();
+    }
 })(jQuery)
