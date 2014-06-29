7e12000005daeae4c56a066f95f1815f0d2f339b
qcy1121 <qcy1121@gmail.com>
2014-06-29 23:58:08 +0800

    add new app

---
+++ jsProjects/app/heart/Koalas to the Max dot Com_files/d3.mouse.patch.js
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+<HTML><HEAD><TITLE>您访问的页面不存在或进行改版 请转到首页进入</TITLE>
+<META http-equiv=Content-Type content="text/html; charset=GB2312">
+<META http-equiv=refresh content="2;URL = http://www.wangqianfang.cn">
+<META http-equiv=Content-Type content="text/html; charset=gb2312"><LINK 
+href="404_0/ad.css" type=text/css rel=stylesheet>
+<STYLE type=text/css>BODY {
+	BACKGROUND-IMAGE: url('bg.jpg')
+}
+</STYLE>
+<META content="Microsoft FrontPage 6.0" name=GENERATOR></HEAD>
+<BODY>
+<TABLE cellSpacing=0 cellPadding=0 width=600 align=center border=0>
+  <TBODY>
+  <TR>
+    <TD colSpan=3 height=170>　</TD></TR>
+  <TR>
+    <TD width=346>　</TD>
+    <TD width=140 height=140>
+      
+
+
+
+</TD>
+    <TD width=127>　</TD></TR>
+
+  <TR>
+    <TD rowSpan=2>　</TD>
+    <TD colSpan=2 
+      height=120><STRONG>您访问的页面有误,或已删除！</STRONG><BR><BR><BR>5秒钟后跳转至着页<BR><BR>若您有什么意见活着建议可以留言</A><BR>或者直接与我们取得联系，谢谢！<BR>
+      <a href="http://www.wangqianfang.cn/" target="_blank">www.wangqianfang.cn</a>      <A 
+      href="http://wpa.qq.com/msgrd?V=1&amp;Uin=534929488&amp;Site=inme&amp;Menu=yes" 
+      target=blank><IMG height=17 alt=点击！直接和管理对话或留言（无验证） 
+      src="16_offline.gif" width=77 border=0></A><BR>
+      <BR>
+      <SCRIPT language=JavaScript> 
+    var __cc_uid="qzone";var __cc_style = 1 
+    </SCRIPT>
+
+    </SCRIPT>
+    </TD></TR>
+  <TR>
+    <TD vAlign=bottom colSpan=2 height=120>　</TD></TR></TBODY></TABLE></BODY></HTML>
